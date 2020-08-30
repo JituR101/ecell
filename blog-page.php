7aa801badd1e81d0db52d6b7d564aad97eefe532
niharika <niharikamadke@gmail.com>
2020-08-30 15:20:16 +0530

    header-footer of blog

---
+++ blog-page.php
@@ -0,0 +1,247 @@
+<header class="navbar-fixed-top s-header js__header-sticky js__header-overlay">
+            <!-- Navbar -->
+            <div class="s-header__navbar">
+                <div class="s-header__container">
+                    <div class="s-header__navbar-row">
+                        <div class="s-header__navbar-row-col">
+                            <!-- Logo -->
+                            <div class="s-header__logo">
+                                <a href="#" class="s-header__logo-link">
+                                    <img class="s-header__logo-img s-header__logo-img-default" src="img/logo-ecell.png" alt="Ecell Logo" width="135px">
+                                    <img class="s-header__logo-img s-header__logo-img-shrink" src="img/logo-ecell-sm.png" alt="Ecell Logo">
+                                </a>
+                            </div>
+                            <!-- End Logo -->
+                        </div>
+                        <div class="s-header__navbar-row-col">
+                            <!-- Trigger -->
+                            <a href="javascript:void(0);" class="s-header__trigger js__trigger">
+                                <span class="s-header__trigger-icon"></span>
+                                <svg x="0rem" y="0rem" width="3.125rem" height="3.125rem" viewbox="0 0 54 54">
+                                    <circle fill="transparent" stroke="#fff" stroke-width="1" cx="27" cy="27" r="25" stroke-dasharray="157 157" stroke-dashoffset="157"></circle>
+                                </svg>
+                            </a>
+                            <!-- End Trigger -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Navbar -->
+
+            <!-- Overlay -->
+            <div class="s-header-bg-overlay js__bg-overlay" id="overlay">
+                <!-- Nav -->
+                <nav class="s-header__nav js__scrollbar">
+                    <div class="container-fluid">
+
+                        <!-- Menu List -->
+                        <ul class="list-unstyled s-header__nav-menu">
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="#about">About</a></li>
+
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="#flagship">Events</a></li>
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="spons.php">Sponsors</a></li>
+				            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="https://startupconclave.ecellvnit.org">Startup Conclave</a></li>
+				            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="team.php">Team</a></li>
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="#blog">Blog</a></li>
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="#contact">Contact Us</a></li>
+                                            
+                        </ul>
+                        <!-- End Menu List -->
+                    </div>
+                </nav>
+                <!-- End Nav -->
+
+
+                <!-- Action -->
+                <ul class="list-inline s-header__action s-header__action--rb">
+                    <li class="s-header__action-item">
+                        <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/">
+                            <i class="g-padding-r-5--xs ti-facebook"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>
+                        </a>
+                    </li>
+                    <li class="s-header__action-item">
+                        <a class="s-header__action-link" href="https://twitter.com/ecell_vnit">
+                            <i class="g-padding-r-5--xs ti-twitter"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Twitter</span>
+                        </a>
+                    </li>
+                    <li class="s-header__action-item">
+                        <a class="s-header__action-link" href=" https://www.instagram.com/ecellvnit/">
+                            <i class="g-padding-r-5--xs ti-instagram"></i>
+                            <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
+                        </a>
+                    </li>
+                </ul>
+                <!-- End Action -->
+            </div>
+            <!-- End Overlay -->
+        </header>
+        <!--========== END HEADER ==========-->
+ 
+        <footer class="g-bg-color--dark">
+            <!-- Sponsors -->
+            <div class="footer">
+                <div class="g-text-center--xs g-margin-b-40--xs">
+                    <!--<p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Partners</p>-->
+                    <h2 class="g-font-size-24--xs g-font-size-36--md g-color--white">Under patronage of</h2>
+                </div>
+                <div class="container g-padding-y-40--xs g-padding-y-40--sm" id="partners">
+                    <!-- Swiper Clients -->
+                    <div class="container" id="foot">
+                        <div class="row">
+
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".1s">
+                                    <img class="s-clients-v1" src="img/clients/Make_In_India.png" alt="Clients Logo">
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".3s">
+                                    <img class="s-clients-v1" src="img/clients/TiE.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".4s">
+                                    <img class="s-clients-v1" src="img/clients/CIVN.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".5s">
+                                    <img class="s-clients-v1" src="img/clients/pmyuva.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".2s">
+                                    <img class="s-clients-v1" src="img/clients/digital-india.png" alt="Clients Logo">
+                                </di                       </div>
+                            <div class="col-md-2 col-sm-3 col-xs-6">
+                                <div class="wow fadeIn" data-wow-duration=".3" data-wow-delay=".5s">
+                                    <img class="s-clients-v1" src="img/clients/vnit_aa.png" alt="Clients Logo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Swiper Clients -->
+                </div>
+            </div>
+            <!-- End Sponsors -->
+            <!-- Copyright -->
+            <div class="container g-padding-y-30--xs">
+                <h1 class="g-color--white-opacity">Quick Links</h1>
+                <br>
+                <div class="row">
+
+                    <div class="col-sm-12 col-md-3 col-lg-3">
+                        <h4 class="g-color--white-opacity"><b>Consortium'19</b></h4>
+                        <a href="https://ca.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+                           Consortium, the Annual Entrepreneurship Summit of VNIT Nagpur
+                        </a>
+                    </div>
+
+                    <div class="col-sm-12 col-md-3 col-lg-3">
+                        <h4 class="g-color--white-opacity"><b>Startup Conclave</b></h4>
+                        <a href="https://startupconclave.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+                            StartUp Conclave, a national-level business plan competition and a part of Consortium'19
+                        </a>
+                    </div>
+
+
+                    <div class="col-sm-12 col-md-3 col-lg-3">
+                        <h4 class="g-color--white-opacity"><b>Buy Merchandise</b></h4>
+                        <a href="https://store.ecellvnit.org/" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+                            Buy official merchandise of Consortium'19 at Ecell VNIT online store.
+                        </a>
+                    </div>
+                    <div class="col-sm-12 col-md-3 col-lg-3">
+                        <h4 class="g-color--white-opacity"><b>Startup Expo</b></h4>
+                        <a href="expo.php" target="_blank" class="g-color--white-opacity-light footer-para g-font-size-16--xs">
+                            A grand platform for startups to display their product and/ or service to all the people.
+                        </a>
+                    </div>
+
+
+
+                </div>
+            </div>
+            <br>
+
+
+            <div class="container" style="padding-top:22px">
+
+                        <div class="row">
+
+
+
+                            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+                                <div>
+                                    <img src="https://startupconclave.ecellvnit.org/static/img/E-Cell_white.png" alt="" height="80" style="padding-top:10px">
+
+                                </div>
+
+                            </div>
+                            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+                                <h4 class="g-color--white-opacity">Contact Us</h4>
+                                    <p class="g-color--white-opacity-light">For Association Opportunities:<br><b>contact@ecellvnit.org<b></p>
+                                    <p class="g-color--white-opacity-light">For Sponsorship Opportunities:<br><b>corporate@ecellvnit.org</b></p>
+                            </div>
+                            <div class="col-sm-12 col-sm-4 col-md-4 col-lg-4">
+
+                                <h4 class="g-color--white-opacity">Follow Ecell VNIT</h4>
+
+                                <a class="s-header__action-link" href=" https://m.facebook.com/vnitecell/" target="_blank">
+                                    <i class="g-padding-r-5--xs ti-facebook"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">Facebook</span>
+                                </a>
+                                <a class="s-header__action-link" href="https://twitter.com/ecell_vnit" target="_blank">
+                                    <i class="g-padding-r-5--xs ti-twitter"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">Twitter</span>
+                                </a>
+                                <a class="s-header__action-link" href=" https://www.instagram.com/ecellvnit/" target="_blank">
+                                    <i class="g-padding-r-5--xs ti-instagram"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">Instagram</span>
+                                </a>
+
+
+                                <a class="s-header__action-link" href=" https://www.linkedin.com/company/6615520/" target="_blank">
+                                    <i class="g-padding-r-5--xs ti-linkedin"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">LinkedIn</span>
+                                </a>
+
+                                <a class="s-header__action-link" href=" https://www.youtube.com/channel/UC0KNOmODhqLcEpcfN9qEsIQ" target="_blank">
+                                    <i class="g-padding-r-5--xs ti-youtube"></i>
+                                    <span class="g-display-none--xs g-display-inline-block--sm">YouTube</span>
+                                </a>
+
+                            </div>
+                            <div class="col-sm-12 col-sm-4 col-md-4 col-lg-2">
+                              <h4 class="g-color--white-opacity">Our Hosting Partner</h4>
+                              <div>
+
+                              </a href="https://www.znetlive.com/">
+
+                                  <img src="img/znetlive.png" alt="" height="140" style="padding-top:5px;padding-left:40px;">
+                                </a>
+                              </div>
+                            </div>
+
+
+
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-12" style="position:relative; text-align:center">
+
+                                <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity g-padding-y-20--md">Copyright:  <a class="g-color--primary" href="https://ecellvnit.org" target="_blank">E-Cell VNIT</a>    Developed By: <a class="g-color--primary" href="https://www.linkedin.com/in/sagar-bansal-448097140/" target="_blank">sagban</a></p>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+            <!-- End Copyright -->
+        </footer>
+
+
